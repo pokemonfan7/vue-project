65b05dec9d3b09944f001b32741e470bb513cfd6
Alexz <rumblesoso7@gmail.com>
2020-01-20 15:53:44 +0800

    feat: use less and edit main layout

--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -1,6 +1,15 @@
-<script src="../../router/index.js"></script>
 <template>
-  <div>这是主要页面</div>
+  <div class="container">
+    <!--    <img src="../../assets/logo.png" alt="">-->
+    <!--    <div>这是主要页面</div>-->
+    <header>Header</header>
+    <div class="content">
+      <aside>Aside</aside>
+      <main>
+        <el-button type="primary">主要按钮</el-button>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +18,32 @@
 	}
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .container {
+    width: 100%;
+    height: 100%;
+  }
 
+  header {
+    height: 50px;
+    background-color: lightgreen;
+  }
+
+  .content {
+    display: flex;
+    width: 100%;
+    height: 100%;
+
+    aside {
+      width: 200px;
+      height: 100%;
+      background-color: lightblue;
+    }
+
+    main {
+      flex: 1 0 auto;
+      height: 100%;
+      background-color: lightgray;
+    }
+  }
 </style>
